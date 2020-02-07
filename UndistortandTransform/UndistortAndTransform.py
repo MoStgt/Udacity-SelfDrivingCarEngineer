5ee72da513efb63d0d12855c4c671b27d03dae7b
Mohamed <mohamad_mohamed@hotmail.de>
2020-02-08 00:16:34 +0100

    Create file

--- UndistortandTransform/UndistortAndTransform.py
+++ UndistortandTransform/UndistortAndTransform.py
@@ -1,4 +1,24 @@
-# Define a function that takes an image, number of x and y points, 
+import pickle
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+
+# Read in the saved camera matrix and distortion coefficients
+# These are the arrays you calculated using cv2.calibrateCamera()
+dist_pickle = pickle.load( open( "wide_dist_pickle.p", "rb" ) )
+mtx = dist_pickle["mtx"]
+dist = dist_pickle["dist"]
+
+# Read in an image
+img = cv2.imread('test_image2.png')
+nx = 8 # the number of inside corners in x
+ny = 6 # the number of inside corners in y
+
+
+# Define a function that takes an image, number of x and y points,
 # camera matrix and distortion coefficients
 def corners_unwarp(img, nx, ny, mtx, dist):
     # Use the OpenCV undistort() function to remove distortion
@@ -33,3 +53,13 @@ def corners_unwarp(img, nx, ny, mtx, dist):
 
     # Return the resulting image and matrix
     return warped, M
+
+top_down, perspective_M = corners_unwarp(img, nx, ny, mtx, dist)
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(img)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(top_down)
+ax2.set_title('Undistorted and Warped Image', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()

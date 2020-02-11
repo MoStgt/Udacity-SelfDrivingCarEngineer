cf2f9bbb89c884720052a896e086d2186c2fe923
Mohamed <mohamad_mohamed@hotmail.de>
2020-02-11 22:28:57 +0100

    create files

--- AdvancedLaneFinding/AdvancedLaneFindingHistogram.py
+++ AdvancedLaneFinding/AdvancedLaneFindingHistogram.py
@@ -10,7 +10,7 @@ img = mpimg.imread('warped-example.jpg') / 255
 def hist(img):
     # TO-DO: Grab only the bottom half of the image
     # Lane lines are likely to be mostly vertical nearest to the car
-    bottom_half = img[img.shape[0]//2:,:]
+    bottom_half = img[img.shape[0] // 2:,:]
 
     # TO-DO: Sum across image pixels vertically - make sure to set `axis`
     # i.e. the highest areas of vertical lines should be larger values
